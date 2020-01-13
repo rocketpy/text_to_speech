537fcd41aa6ae8c81275ca481361823b6eb49e23
rocketpy <dimitr.ushakov@gmail.com>
2020-01-13 21:36:19 +0200

    modified

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,1 +1,16 @@
+#  pip install gtts
 
+import os
+from gtts import gTTS
+
+
+text = "Hello world"
+
+print("Wait for result ..")
+TTS = gTTS(text=text, lang='en-uk')
+
+#  save to mp3 in current directory
+TTS.save("new_voice.mp3")
+
+#  play mp3 using the default app on your system
+os.system("start new_voice.mp3")
