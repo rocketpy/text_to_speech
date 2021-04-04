c006ffcd2d9e1452bbdb2964a232d21f88846792
rocketpy <dimitr.ushakov@gmail.com>
2021-04-04 18:49:01 +0300

    Update TextToSpeech_changing_voice.py

--- TextToSpeech_changing_voice.py
+++ TextToSpeech_changing_voice.py
@@ -2,7 +2,7 @@
 # pip install pypiwin32
 
 import pyttsx3
-  
+
 
 converter = pyttsx3.init()
 
@@ -10,7 +10,7 @@ converter = pyttsx3.init()
 converter.setProperty('rate', 150)
 # Set volume 0-1
 converter.setProperty('volume', 0.7)
-  
+
 # Queue the entered text 
 # There will be a pause between
 # each one like a pause in 
@@ -24,5 +24,19 @@ converter.say("I'm a robot")
 converter.runAndWait()
 
 #  Changing Voice
+voices = converter.getProperty('voices')
 
+for voice in voices:
+    # to get the info. about various voices in our PC 
+    print("Voice:")
+    print("ID: %s" %voice.id)
+    print("Name: %s" %voice.name)
+    print("Age: %s" %voice.age)
+    print("Gender: %s" %voice.gender)
+    print("Languages Known: %s" %voice.languages)
+    
+voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+# Use female voice
+converter.setProperty('voice', voice_id)
+converter.runAndWait()
 
